61fb640a89fe1cb2b80c75db849675e2536e1ab7
inaamul-hussain <inaamulhussain@gmail.com>
2024-06-06 12:39:20 +0530

    q7 done

---
+++ Q7/Q7.js
@@ -0,0 +1,10 @@
+let st1={
+    name:"john",
+    age:25,
+}
+
+function greet(pr) {
+    
+    console.log("hello,"+" my name is "+ pr.name + "and I am "+ pr.age + " years old.");   
+}
+greet(st1)
